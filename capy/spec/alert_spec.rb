9ce1b463704bee26ac346ed339d02a61b17ba4f9
thitoribeiro <thito.ribeiro@hotmail.com>
2019-03-15 10:23:06 -0300

    Js alerts lesson

---
+++ capy/spec/alert_spec.rb
@@ -0,0 +1,46 @@
+describe 'Alertas de JS', :alerts do
+    before(:each) do
+      visit 'https://training-wheels-protocol.herokuapp.com/javascript_alerts'
+    end
+  
+    it 'alerta' do
+      click_button 'Alerta'
+  
+      msg = page.driver.browser.switch_to.alert.text
+      expect(msg).to eql 'Isto é uma mensagem de alerta'
+  
+      page.driver.browser.switch_to.alert.accept
+    end
+  
+    it 'sim confirma' do
+      click_button 'Confirma'
+  
+      msg = page.driver.browser.switch_to.alert.text
+      expect(msg).to eql 'E ai confirma?'
+  
+      page.driver.browser.switch_to.alert.accept
+      expect(page).to have_content 'Mensagem confirmada'
+    end
+  
+    it 'não confirma' do
+      click_button 'Confirma'
+  
+      msg = page.driver.browser.switch_to.alert.text
+      expect(msg).to eql 'E ai confirma?'
+  
+      page.driver.browser.switch_to.alert.dismiss
+      expect(page).to have_content 'Mensagem não confirmada'
+    end
+  
+    it 'accept prompt', :accept_prompt do
+      accept_prompt(with: 'Fernando') do
+        click_button 'Prompt'
+      end
+  
+      expect(page).to have_content 'Olá, Fernando'
+    end
+  
+      it 'dismiss prompt', :dismiss_prompt do
+   
+    end
+  end
