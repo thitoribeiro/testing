2e2e0da78f2f6252fd209e8b1f5a469715dc5108
thitoribeiro <thito.ribeiro@hotmail.com>
2019-03-19 10:54:50 -0300

    Iframe lesson

--- capy/spec/login3_spec.rb
+++ capy/spec/login3_spec.rb
@@ -9,17 +9,21 @@ describe 'Login com Cadastro', :login3 do
       find('input[name=password]').set 'jarvis!'
       click_button 'Entrar'
     end
+      
 
-    expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
+    #expect(page).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
+    #expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
+    
   end
 
   it 'cadastro com sucesso' do
     within('#signup') do
-      find('input[name=username]').set 'feranndo'
+      find('input[name=username]').set 'thito'
       find('input[name=password]').set '123456'
       click_link 'Criar Conta'
     end
+      
 
-    expect(page).to have_content 'Dados enviados. Aguarde aprovação do seu cadastro!'
+    #expect(page).to have_content 'Dados enviados. Aguarde aprovação do seu cadastro!'
   end
 end
