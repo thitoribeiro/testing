7c0c8dd33d694f5ea2cfb8165d17f76f3f46bab2
thitoribeiro <thito.ribeiro@hotmail.com>
2019-08-31 20:24:09 -0300

    autocomplete file

---
+++ tau/rb/keyboard_mouse.rb
@@ -0,0 +1,12 @@
+require "selenium-webdriver"
+
+driver = Selenium::WebDriver.for:chrome
+driver.navigate.to "https://formy-project.herokuapp.com/keypress"
+
+name = driver.find_element(id: 'name')
+name.send_keys('Thito Ribeiro')
+
+button = driver.find_element(id: 'button')
+button.click
+
+sleep 5
