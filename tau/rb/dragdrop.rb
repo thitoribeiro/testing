dfa43e06b30c99149ec05725d89b87a69b43e145
thitoribeiro <thito.ribeiro@hotmail.com>
2019-09-01 09:35:18 -0300

    grag and drop and radio button files

---
+++ tau/rb/dragdrop.rb
@@ -0,0 +1,10 @@
+require "selenium-webdriver"
+
+driver = Selenium::WebDriver.for :chrome
+driver.navigate.to "https://formy-project.herokuapp.com/dragdrop"
+
+image = driver.find_element(id: 'image')
+box = driver.find_element(id: 'box')
+sleep 2
+driver.action.drag_and_drop(image,box).perform
+sleep 2
