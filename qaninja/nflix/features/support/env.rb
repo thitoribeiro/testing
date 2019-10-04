de4e41459e0182fd6a3de1ced786d0f0833a1edd
thitoribeiro <thito.ribeiro@hotmail.com>
2019-10-04 20:44:00 -0300

    Gemfile and env file Nflix project

---
+++ qaninja/nflix/features/support/env.rb
@@ -0,0 +1,10 @@
+require "capybara"
+require "capybara/cucumber"
+require "selenium-webdriver"
+
+Capybara.configure do |config|
+    config.default_driver = :selenium
+    config.app_host = "http://localhost:8080"
+end
+
+
