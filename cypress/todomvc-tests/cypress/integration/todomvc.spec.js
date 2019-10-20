4360f8b2f3932245f7cd0d4cfe77bec6e52534b0
thitoribeiro <thito.ribeiro@hotmail.com>
2019-10-20 19:34:36 -0300

    Cypress .visit, .get and timeout class

--- cypress/todomvc-tests/cypress/integration/todomvc.spec.js
+++ cypress/todomvc-tests/cypress/integration/todomvc.spec.js
@@ -1,5 +1,8 @@
 ///<reference types="cypress" />
 
-it('should navigate to the TodoMVC App', () => {
-    cy.visit('http://todomvc-app-for-testing.surge.sh/')
+it('should b able to add a new todo to the list', () => {
+    cy.visit('http://todomvc-app-for-testing.surge.sh/?delay-new-todo=5000')
+    
+    cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}")
+
 })
