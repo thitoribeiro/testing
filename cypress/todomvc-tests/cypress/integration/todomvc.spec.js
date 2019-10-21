966661f0376a9366d72a5cc54389dea3879fd2e5
thitoribeiro <thito.ribeiro@hotmail.com>
2019-10-21 17:16:52 -0300

    Another cypress class

--- cypress/todomvc-tests/cypress/integration/todomvc.spec.js
+++ cypress/todomvc-tests/cypress/integration/todomvc.spec.js
@@ -1,8 +1,17 @@
 ///<reference types="cypress" />
 
 it('should b able to add a new todo to the list', () => {
-    cy.visit('http://todomvc-app-for-testing.surge.sh/?delay-new-todo=5000')
+    cy.visit('http://todomvc-app-for-testing.surge.sh/')
     
     cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}")
+    
+    cy.get('label').should('have.text', 'Clean room')
+    cy.get('.toggle').should('not.be.checked')
+    
+    cy.get('.toggle').click()
+    cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
+
+    cy.contains('Clear').click()
+    cy.get('.todo-list').should('not.have.descendants', 'li')
 
 })
