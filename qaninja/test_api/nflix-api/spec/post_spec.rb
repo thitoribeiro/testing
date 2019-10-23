4737883fdd562fc89db469045c7ed5c2471c99d4
thitoribeiro <thito.ribeiro@hotmail.com>
2019-10-22 22:01:00 -0300

    File class updated

--- qaninja/test_api/nflix-api/spec/post_spec.rb
+++ qaninja/test_api/nflix-api/spec/post_spec.rb
@@ -1,16 +1,23 @@
-describe "cadastro" do
-    it "novo usuario" do
 
-        Database.new.delete_user("thito.ribeiro@hotmail.com")
+describe "post" do
+    context "quando cadastro um novo usuário" do
+        before do
+            @new_user = { full_name: "Thito Ribeiro", email: "thito.ribeiro@hotmail.com", password: "pwd123"}
+            Database.new.delete_user(@new_user[:email])
 
-        result = HTTParty.post(
-            "http://localhost:3000/user", 
-            body: {full_name: "Thito Ribeiro", email: "thito.ribeiro@hotmail.com", password: "pwd123"}.to_json,
-            headers: {
-                "Content-Type" => "application/json",
-            },
-        )
-        puts result
-        expect(result.response.code).to eql "200"
+            @result = HTTParty.post(
+                "http://localhost:3001/user",
+                body: @new_user.to_json,
+                headers: {
+                    "Context-Type" => "application/json",
+                },
+            )
+        end
+
+        it "então deve retornar 200" do
+            puts @result
+            expect(@result.response.code).to eql "200"
+        end
     end
 end
+
