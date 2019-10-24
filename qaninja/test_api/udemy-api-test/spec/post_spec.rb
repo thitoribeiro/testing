8e8410bbbb9615fc6f8e3c3b054d10402e99fc0d
thitoribeiro <thito.ribeiro@hotmail.com>
2019-10-23 23:18:42 -0300

    Begining post file

---
+++ qaninja/test_api/udemy-api-test/spec/post_spec.rb
@@ -0,0 +1,9 @@
+describe 'fazer uma requisição' do
+    it 'get' do
+      #@result = HTTParty.get('https://jsonplaceholder.typicode.com/users/1')
+      #puts @result
+  
+      @new_user = User.get('/users/1')
+      puts @new_user
+    end
+  end
