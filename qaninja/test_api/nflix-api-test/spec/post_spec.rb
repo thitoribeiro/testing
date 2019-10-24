5638318e1b7980a932814445d71b7255c474adf6
thitoribeiro <thito.ribeiro@hotmail.com>
2019-10-23 22:29:19 -0300

    Updated file

--- qaninja/test_api/nflix-api-test/spec/post_spec.rb
+++ qaninja/test_api/nflix-api-test/spec/post_spec.rb
@@ -4,16 +4,8 @@ describe "post" do
   context "when new user" do
     before do
       @new_user = build(:user).to_hash
-
-      @result = HTTParty.post(
-        "http://localhost:3001/user",
-        body: @new_user.to_json,
-        headers: {
-          "Content-Type" => "application/json",
-        },
-      )
+      @result = ApiUser.save(@new_user)
     end
-
     it { expect(@result.response.code).to eql "200" }
   end
 
