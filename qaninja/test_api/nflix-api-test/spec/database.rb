ddc468680702ad430b5d66f5a50b628eaca5bf34
thitoribeiro <thito.ribeiro@hotmail.com>
2019-10-25 15:28:02 -0300

    Updated files

--- qaninja/test_api/nflix-api-test/spec/database.rb
+++ qaninja/test_api/nflix-api-test/spec/database.rb
@@ -2,8 +2,10 @@ require "pg"
 
 class Database
   def initialize
-    conn = { host: "http://localhost:3001/user", dbname: "nflix", user: "postgres", password: "qaninja" }
-    @connection = PG.connect(conn)
+    @connection = conn = PG::Connection.open(:host => 'http://localhost:3001/user', :dbname => 'nflix', :user => 'postgres', :password => 'qaninja')
+    #res = conn.exec_params(:host => 'http://localhost:3001/user')
+    #conn = { 'host: "http://localhost:3001/user", dbname: "nflix", user: "postgres", password: "qaninja" }
+    #@connection = PG.connect(conn)
   end
 
   def delete_user(email)
