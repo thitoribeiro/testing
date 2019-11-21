3e2d64fa01b65b302acdfaa1670ac3e99eaf3839
thitoribeiro <thito.ribeiro@hotmail.com>
2019-11-20 21:11:05 -0300

    Updated file

--- qaninja/rspec-test/spec/saque_spec.rb
+++ qaninja/rspec-test/spec/saque_spec.rb
@@ -4,8 +4,8 @@ describe ContaCorrente do
   describe 'Saque' do
     context 'quando o valor é positivo' do
       before(:all) do
-        @conta = ContaCorrente.new(1000.00)
-        @conta.saca(200)
+        @cc = ContaCorrente.new(1000.00)
+        @cc.saca(200)
       end
       it 'entao atualiza saldo' do
         expect(@conta.saldo).to eql 800.00
@@ -14,38 +14,38 @@ describe ContaCorrente do
 
     context 'quando o saldo é zero' do
       before(:all) do
-        @conta = ContaCorrente.new(0.00)
-        @conta.saca(100.00)
+        @cc = ContaCorrente.new(0.00)
+        @cc.saca(100.00)
       end
       it 'então exibe mensagem' do
-        expect(@conta.mensagem).to eql 'Saldo insuficiente para saque :('
+        expect(@cc.mensagem).to eql 'Saldo insuficiente para saque :('
       end
       it 'e o saldo final com zero' do
-        expect(@conta.saldo).to eql 0.00
+        expect(@cc.saldo).to eql 0.00
       end
     end
     context 'quando o saldo é insuficiente' do
       before(:all) do
-        @conta = ContaCorrente.new(100.00)
-        @conta.saca(101.00)
+        @cc = ContaCorrente.new(100.00)
+        @cc.saca(101.00)
       end
       it 'então exibe mensagem' do
-        expect(@conta.mensagem).to eql 'Saldo insuficiente para saque :('
+        expect(@cc.mensagem).to eql 'Saldo insuficiente para saque :('
       end
       it 'e o saldo permanece' do
-        expect(@conta.saldo).to eql 100.00
+        expect(@cc.saldo).to eql 100.00
       end
     end
     context 'quando supera o limite de saque' do
       before(:all) do
-        @conta = ContaCorrente.new(1000.00)
-        @conta.saca(701.00)
+        @cc = ContaCorrente.new(1000.00)
+        @cc.saca(701.00)
       end
       it 'então exibe mensagem' do
-        expect(@conta.mensagem).to eql 'Limite máximo por saque é de R$700'
+        expect(@cc.mensagem).to eql 'Limite máximo por saque é de R$700'
       end
       it 'e o saldo permanece' do
-        expect(@conta.saldo).to eql 1000.00
+        expect(@cc.saldo).to eql 1000.00
       end
     end
   end
