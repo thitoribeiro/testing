5dd091471eecb91421819a3012487883ffae410e
thitoribeiro <thito.ribeiro@hotmail.com>
2019-11-20 21:08:53 -0300

    Updated files

--- qaninja/rspec-test/spec/saque_spec.rb
+++ qaninja/rspec-test/spec/saque_spec.rb
@@ -1,16 +1,4 @@
-class ContaCorrente
-  attr_accessor :saldo
-  
-  def initialize(saldo)
-    self.saldo = saldo
-  end
-
-  def saca(valor)
-    self.saldo -= valor
-    puts saldo
-  end
-  
-end
+require_relative '../app/bank'
 
 describe ContaCorrente do
   describe 'Saque' do
@@ -21,6 +9,43 @@ describe ContaCorrente do
       end
       it 'entao atualiza saldo' do
         expect(@conta.saldo).to eql 800.00
+      end
+    end
+
+    context 'quando o saldo é zero' do
+      before(:all) do
+        @conta = ContaCorrente.new(0.00)
+        @conta.saca(100.00)
+      end
+      it 'então exibe mensagem' do
+        expect(@conta.mensagem).to eql 'Saldo insuficiente para saque :('
+      end
+      it 'e o saldo final com zero' do
+        expect(@conta.saldo).to eql 0.00
+      end
+    end
+    context 'quando o saldo é insuficiente' do
+      before(:all) do
+        @conta = ContaCorrente.new(100.00)
+        @conta.saca(101.00)
+      end
+      it 'então exibe mensagem' do
+        expect(@conta.mensagem).to eql 'Saldo insuficiente para saque :('
+      end
+      it 'e o saldo permanece' do
+        expect(@conta.saldo).to eql 100.00
+      end
+    end
+    context 'quando supera o limite de saque' do
+      before(:all) do
+        @conta = ContaCorrente.new(1000.00)
+        @conta.saca(701.00)
+      end
+      it 'então exibe mensagem' do
+        expect(@conta.mensagem).to eql 'Limite máximo por saque é de R$700'
+      end
+      it 'e o saldo permanece' do
+        expect(@conta.saldo).to eql 1000.00
       end
     end
   end
