25d36e57d86239082123e0af05fa8ed3bbe094a4
thitoribeiro <thito.ribeiro@hotmail.com>
2019-12-04 22:52:13 -0300

    Udemy codeceptjs course first class

---
+++ codeceptjs/e2e-test/codecept.conf.js
@@ -0,0 +1,16 @@
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    WebDriver: {
+      url: 'http://the-internet.herokuapp.com/',
+      browser: 'chrome'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'e2e-test'
+}
