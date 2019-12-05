5da6084a86a2eeaab5997bb19690b1aa9af65fe2
thitoribeiro <thito.ribeiro@hotmail.com>
2019-12-05 14:50:48 -0300

    Project update

---
+++ codeceptjs/e2e-test/editor_test.js
@@ -0,0 +1,15 @@
+
+Feature('Editor');
+
+Scenario('Can be opened and the text input and bold formatting is working', (I) => {
+    I.amOnPage('/');
+    I.click('WYSIWYG Editor');
+    I.switchTo('#mce_0_ifr');
+    I.fillField('#tinymce', 'My text bold');
+    I.doubleClick('#tinymce');
+    I.switchTo();
+    I.click('#mceu_3');
+    I.switchTo('#mce_0_ifr');
+    I.click('#tinymce');
+    I.saveScreenshot('editor_test.png')   
+});
