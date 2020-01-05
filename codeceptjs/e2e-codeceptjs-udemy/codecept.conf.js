77a78ce0b8fd447ad6794039c0a4f3222847513e
thitoribeiro <thito.ribeiro@hotmail.com>
2020-01-05 19:57:36 -0300

    e2e codeceptjs test Browserstack

--- codeceptjs/e2e-codeceptjs-udemy/codecept.conf.js
+++ codeceptjs/e2e-codeceptjs-udemy/codecept.conf.js
@@ -1,16 +1,29 @@
 exports.config = {
-  tests: './*_test.js',
-  output: './output',
+  tests: "./*_test.js",
+  output: "./output",
   helpers: {
-    WebDriver: {
-      url: 'http://the-internet.herokuapp.com',
-      browser: 'chrome'
-    }
+  WebDriver: {
+  url: "http://the-internet.herokuapp.com",
+  browser: "safari", //Browser to be tested
+  desiredCapabilities: { //Browserstack settings
+  browser_version: "12",
+  resolution: "1920x1080",
+  project: "practical-e2e-testing"
+  },
+  user: "thitoribeiro1", //Browserstack user
+  key: "Ep2R2nYAKpFh7JTLejkn" //Brouserstack key
+  },
+  BrowserstackHelper: {
+  require: "codeceptjs-bshelper",
+  user: "thitoribeiro1",
+  key: "Ep2R2nYAKpFh7JTLejkn"
+  },
+  REST: {}
   },
   include: {
-    I: './steps_file.js'
+  I: "./steps_file.js"
   },
   bootstrap: null,
   mocha: {},
-  name: 'e2e-codeceptjs-udemy'
-}
+  name: "e2e-codeceptjs-udemy",
+  };
