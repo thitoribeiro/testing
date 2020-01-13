4998f93431f4995036529404972e4ed2aea5b93a
thitoribeiro <thito.ribeiro@hotmail.com>
2020-01-13 18:45:57 -0200

    Add chrome ci test job.

--- codeceptjs/codeceptjs-udemy/first-e2e-test/codecept.conf.js
+++ codeceptjs/codeceptjs-udemy/first-e2e-test/codecept.conf.js
@@ -1,16 +1,21 @@
-exports.config = {
+let config = {
   tests: "./*_test.js",
   output: "./output",
   helpers: {
     WebDriver: {
       url: "http://the-internet.herokuapp.com",
       browser: "chrome",
-},
-REST: {} },
+      windowSize: "maximize"
+} },
   include: {
     I: "./steps_file.js"
   },
   bootstrap: null,
   mocha: {},
-  name: "e2e-tests",
+  name: "first-e2e-test",
 };
+if (process.profile === "chrome-ci") {
+config.helpers.WebDriver.host = process.env.SELENIUM_STANDALONE_CHROME_PORT_4444_TCP_ADDR; config.helpers.WebDriver.protocol = "http";
+config.helpers.WebDriver.port = 4444;
+}
+exports.config = config;
