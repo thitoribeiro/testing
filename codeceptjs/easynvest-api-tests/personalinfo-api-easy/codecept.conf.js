830ce7fa393f8a372e0be37735c6432299f3bcf8
Thito Rodrigo Ribeiro <thito.ribeiro@hotmail.com>
2020-02-05 14:36:01 -0300

    Files updated

---
+++ codeceptjs/easynvest-api-tests/personalinfo-api-easy/codecept.conf.js
@@ -0,0 +1,22 @@
+exports.config = {
+tests: "./*/*_test.js",
+	output: "./output",
+	helpers: {
+		REST: {
+			endpoint: "https://swapi.co",
+			onRequest: () => {
+				
+			}
+		}
+	},
+	include: {},
+	bootstrap: null,
+	mocha: {},
+	name: "Easynvest API Test",
+	plugins: {
+		allure: {
+			outputDir: "report",
+			enabled: false
+		}
+	}
+};
