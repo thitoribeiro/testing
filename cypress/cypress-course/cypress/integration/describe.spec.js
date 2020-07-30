eb6d31b94fb5ba826cddd6c48fbc84368e009163
thitoribeiro <thito.ribeiro@hotmail.com>
2020-07-29 22:57:02 -0300

    Describe and it

---
+++ cypress/cypress-course/cypress/integration/describe.spec.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+it('A external test...', () => {
+      
+})
+
+describe('Should group test...', () => {
+  
+  describe('Should group more specific tests...', () => {
+    it('A specific test...', () => {
+      
+    })
+    
+  })
+
+  describe('Should group more specific tests...', () => {
+    it('Another specific test...', () => {
+      
+    })
+    
+  })
+  
+  
+  
+  
+  it('A internal test...', () => {
+      
+  }) 
+})
