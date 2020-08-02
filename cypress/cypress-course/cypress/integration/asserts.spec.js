89c6927ac40eecce5e7be142a406d8b3f493b27f
thitoribeiro <thito.ribeiro@hotmail.com>
2020-08-02 16:43:10 -0300

    Cypress general asserts

--- cypress/cypress-course/cypress/integration/asserts.spec.js
+++ cypress/cypress-course/cypress/integration/asserts.spec.js
@@ -37,3 +37,48 @@ it('Object equality', () => {
   expect({}).to.be.empty;
   
 })
+
+
+it('Arrays', () => {
+  const arr = [1, 2, 3]
+  expect(arr).to.have.members([1, 2, 3])
+  expect(arr).to.include.members([1, 3])
+  expect(arr).to.not.be.empty
+  expect([]).to.be.empty
+})
+
+it('Types', () => {
+  const num = 1
+  const str = 'String'
+
+  expect(num).to.be.a('number')
+  expect(str).to.be.a('string')
+  expect({}).to.be.an('object')
+  expect([]).to.be.an('array')
+})
+
+it('String', () => {
+  const str = 'String de teste'
+
+  expect(str).to.be.eql('String de teste')
+  expect(str).to.have.length(15)
+  expect(str).to.contains('de')
+  expect(str).to.match(/^String/)
+  expect(str).to.match(/teste$/)
+  expect(str).to.match(/.{15}/)
+  expect(str).to.match(/\w+/)
+  expect(str).to.match(/\D+/)
+
+})
+
+it('Numbers', () => {
+  const number = 4
+  const floatNumber = 5.2123
+
+  expect(number).to.be.eql(4)
+  expect(number).to.be.above(3)
+  expect(number).to.be.below(7)
+  expect(floatNumber).to.be.eql(5.2123)
+  expect(floatNumber).to.be.above(5)
+  
+})
