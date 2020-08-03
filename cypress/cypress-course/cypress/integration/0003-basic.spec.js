0f9262a76c9ebc08c2a08f3ba85c8bc602ac6b1e
thitoribeiro <thito.ribeiro@hotmail.com>
2020-08-03 17:08:22 -0300

    Visiting the page and checking the title

--- cypress/cypress-course/cypress/integration/0003-basic.spec.js
+++ cypress/cypress-course/cypress/integration/0003-basic.spec.js
@@ -3,5 +3,15 @@
 describe('Cypress basics', () => {
   it('Should visit a page and assert title', () => {
     cy.visit('https://www.wcaquino.me/cypress/componentes.html') 
+
+    // const title = cy.title()
+    // console.log(title)
+
+    cy.title().should('be.eql', 'Campo de Treinamento')
+    cy.title().should('contain', 'Campo de Treinamento')
+
+    cy.title()
+      .should('be.eql', 'Campo de Treinamento')
+      .and('contain', 'Campo')
   })
 })
